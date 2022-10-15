9b83015d671bdc585514930956fd12ccb6b14978
Slsachiya99 <115049370+Slsachiya99@users.noreply.github.com>
2022-10-15 15:37:27 +0530

    ♲SACHIYA ♲

---
+++ global.js
@@ -0,0 +1,37 @@
+//global
+
+// ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
+
+//▮ SACHIYA-MD-V2 BOT INC 2022 ®️ALL RIGHTS RESERVED
+
+//▮
+
+//▮FORK AND DON'T FORGET TO GIVE A STAR
+
+//▮
+
+//▮THIS SOFTWARE IS UNDER UZ COPYRIGHT
+
+//▮
+
+//▮
+
+//╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+//
+
+//┏━━━━━━━━━━━━━━━━━━━━━━━━━
+
+//┃THIS SOFTWARE INCLUDES 
+
+//┃SOME ENCRYPTED FILES
+
+//┃
+
+//┃THANKS FOR CHOOSING NIROBOT
+
+//┃THANKS TO DIKA ARDNT
+
+//┗━━━━━━━━━━━━━━━━━━━━━━━━━
+
+//
